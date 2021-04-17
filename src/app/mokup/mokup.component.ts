9b4dd431dcc179d4517026e4184b2da7c4026863
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2021-04-17 21:46:43 +0200

    push

--- src/app/mokup/mokup.component.ts
+++ src/app/mokup/mokup.component.ts
@@ -7,25 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MokupComponent implements OnInit {
 
-  image='../assets/images/mobile.jpg'
-  image2='../assets/images/ordi.jpg'
- mobile:any
- valeur=window.scrollY
+
  
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  getImageMobile(){
-    return this.image
-  }
-  getImageOrdi(){
-    return this.image2
-  }
-
- parralax1(){
-   
-   this.mobile.style.top= this.valeur*1+"px"
- }
 }
