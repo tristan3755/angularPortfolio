c0419a3487d8ddc24b7554c4ab9345c2fd4d6afe
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2021-05-22 15:32:57 +0200

    portfolio

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -7,17 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
-
-  image1:any='../../assets/images/html5.jpg'
+image1:any='../../assets/images/html5.jpg'
 image2:any='../../assets/images/css.jpg'
 image3:any='../../assets/images/js.png'
 image4:any='../../assets/images/Angular.png'
 image10:any='../../assets/images/ai.png'
-image5:any='../../assets/images/blender_logo.png'
+image5:any='../../assets/images/express.png'
 image6:any='../../assets/images/figma.png'
 image7:any='../../assets/images/SVG.jpg'
-image8:any='../../assets/images/mysql.png'
-image9:any='../../assets/images/php.jpg'
+image8:any='../../assets/images/mongo.gif'
+image9:any='../../assets/images/node.png'
 
   constructor() { }
 
