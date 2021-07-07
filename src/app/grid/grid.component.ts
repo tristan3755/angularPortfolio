123bb90ce704806be2736053895432e79d573331
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2021-07-07 16:06:57 +0200

    portfolio

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class GridComponent implements OnInit {
 image1:any='../../assets/images/community.jpg'
 image2:any='../../assets/images/communart.jpg'
-image3:any='../../assets/images/gantColor.jpg'
+image3:any='../../assets/images/metis.jpg'
 image4:any='../../assets/images/the.jpg'
 video:any='../../assets/images/sun.mp4'
 image5:any='../../assets/images/meteo.jpg'
