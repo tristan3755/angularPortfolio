d57cc5af21d0476985365ec94cadd0d52e7bdd16
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2021-08-27 20:03:28 +0200

    push

--- src/assets/sitesPortfolio/communart/peinture.js
+++ src/assets/sitesPortfolio/communart/peinture.js
@@ -13,6 +13,29 @@ let blocArticle=document.createElement('article')
 blocArticle.classList='blocArticle'
 blocArticle.style.backgroundImage='url('+res[i].imageArticle+')'
 
+let blocTitre=document.createElement('p')
+blocTitre.classList='titre'
+
+let dateArticle= res[i].createdAt.split('T')
+console.log(dateArticle)
+
+let date1=dateArticle[0]
+console.log(date1)
+
+let date2=date1.split('-')
+console.log(date2)
+
+let dateFinie=date2[2]+'/'+date2[1]+'/'+date2[0]
+
+blocTitre.innerHTML=res[i].titre + ',article rédigé le : ' + dateFinie
+
+let blocFiltre=document.createElement('div')
+
+blocArticle.appendChild(blocFiltre)
+blocFiltre.classList='blocFiltre'
+
+blocArticle.appendChild(blocTitre)
+
 monGrid.appendChild(blocArticle)
 
 }
