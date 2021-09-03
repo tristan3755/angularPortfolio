777dd1cc6b7570bc05ccf7c11cf8777c08c02cb0
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2021-09-03 12:51:31 +0200

    portfolio refait

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -7,17 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
-image1:any='../../assets/images/html5.jpg'
-image2:any='../../assets/images/css.jpg'
-image3:any='../../assets/images/js.png'
-image4:any='../../assets/images/Angular.png'
-image10:any='../../assets/images/ai.png'
-image5:any='../../assets/images/express.png'
-image6:any='../../assets/images/figma.png'
-image7:any='../../assets/images/SVG.jpg'
-image8:any='../../assets/images/mongo.gif'
-image9:any='../../assets/images/node.png'
-
   constructor() { }
 
   ngOnInit(): void {
